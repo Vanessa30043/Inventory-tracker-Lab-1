0fc19cbdcfec9035e4bf955cfa711ab0c09473b5
Vanessa Sierra <sierrava212@yahoo.com>
2025-11-13 23:36:57 -0500

    added notes to product.ts

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -2,15 +2,20 @@ export class Product{
         sku: string;
         name: string;
         price: number;
+
         constructor (sku:string, name:string , price: number){
             this.sku =sku;
             this.name = name;
             this.price = price;
         }
+
+        //*PRODUCT DETAILS BELOW// METHODS STATED BELOW:
+
         displayDetails():string{
             return`SKU: ${this.sku}, Name: ${this.name}, 
             Price: $${this.price.toFixed(2)}`;
         }   
+        //TAXED 10%=0.1 //
         getPriceWithTax():number{
             return getPriceWithTax(taxRate: number = 0.1): number {
     return this.price * (1 + taxRate);
@@ -19,5 +24,3 @@ export class Product{
 
 
 }
-
-
